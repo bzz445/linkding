3db18a32987283ad074f02154087990d4cee60b1
bzz445 <bzz445@gmail.com>
2023-01-05 14:05:21 +0500

    Initial Commit

---
+++ Linkding/LinkdingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkdingApp.swift
+//  Linkding
+//
+//  Created by Pavel Morozov on 05.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct LinkdingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
